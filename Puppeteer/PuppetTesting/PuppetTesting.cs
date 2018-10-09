d83d18b66528686ee162a9ceeb3afafbdf222012
yrudiev <yrudiev@gmail.com>
2018-10-09 23:02:39 +0300

    Add Have.Component validation

--- Puppeteer/PuppetTesting/PuppetTesting.cs
+++ Puppeteer/PuppetTesting/PuppetTesting.cs
@@ -17,96 +17,10 @@ namespace PuppetTesting
         }
 
         [Test]
-        public void CheckAllMethods()
-        {
-            var gameObject = new GameObject("Main Camera", "Name", "Parent");
-            //gameObject.Click();
-            //gameObject.SendKeys("qwerty");
-            var isExist = gameObject.Exists;
-            //Assert.IsTrue(isExist);
-            var isActive = gameObject.IsActiveInHierarchy;
-            //Assert.IsTrue(isActive);
-            gameObject.ShouldNot(Be.Present);
-            gameObject.ShouldNot(Be.ActiveInHierarchy);
-            //Editor.MakeScreenshot(TestContext.CurrentContext.Test.FullName, "FailedTests");
-         }
-
-        [Test]
-        public void CheckAllMethods2()
-        {
-            var gameObject = new GameObject("Root", "Name", "Parent");
-            gameObject.Click();
-            gameObject.SendKeys("qwerty");
-            var isExist = gameObject.Exists;
-            Assert.IsTrue(isExist);
-            var isActive = gameObject.IsActiveInHierarchy;
-            Assert.IsTrue(isActive);
-            gameObject.Should(Be.Present);
-            gameObject.Should(Be.ActiveInHierarchy);
-            Editor.MakeScreenshot(TestContext.CurrentContext.Test.FullName, "FailedTests");
-        }
-
-        [Test]
-        public void CheckGPLogin()
-        {
-            var LoginGamePointButton = new GameObject("Main Canvas", "gamepointButton");
-            LoginGamePointButton.Should(Be.ActiveInHierarchy);
-            LoginGamePointButton.Click();
-            
-            var UserNameInput = new GameObject("Main Canvas", "loginTextFiled");
-            UserNameInput.Should(Be.ActiveInHierarchy);
-            UserNameInput.Click();
-            UserNameInput.SendKeys("Yevhen");
-            
-            var PasswordInput = new GameObject("Main Canvas", "passTextFiled");
-            PasswordInput.Should(Be.ActiveInHierarchy);
-            PasswordInput.Click();
-            PasswordInput.SendKeys("defaultPassword");
-            
-            var LoginButton = new GameObject("Main Canvas", "button_login");
-            LoginButton.Should(Be.ActiveInHierarchy);
-            LoginButton.Click();
-            
-            var ExperienceBar = new GameObject("Main Canvas", "fillarea_playerxp", "TopBar(Clone)");
-            ExperienceBar.Should(Be.Present);
-            ExperienceBar.Should(Be.ActiveInHierarchy);
-            
-        }
-        
-        [Test]
-        public void CheckGPLogin2()
-        {
-            var LoginGamePointButton = new GameObject("Main Canvas", "gamepointButton");
-            LoginGamePointButton.Should(Be.ActiveInHierarchy);
-            LoginGamePointButton.Click();
-            
-            var UserNameInput = new GameObject("Main Canvas", "loginTextFiled");
-            UserNameInput.Should(Be.ActiveInHierarchy);
-            UserNameInput.Click();
-            UserNameInput.SendKeys("Yevhen");
-            
-            var PasswordInput = new GameObject("Main Canvas", "passTextFiled");
-            PasswordInput.Should(Be.ActiveInHierarchy);
-            PasswordInput.Click();
-            PasswordInput.SendKeys("defaultPassword");
-            
-            var LoginButton = new GameObject("Main Canvas", "button_login");
-            LoginButton.Should(Be.ActiveInHierarchy);
-            LoginButton.Click();
-            
-            var ExperienceBar = new GameObject("Main Canvas", "fillarea_playerxp", "TopBar(Clone)");
-            ExperienceBar.Should(Be.Present);
-            ExperienceBar.Should(Be.ActiveInHierarchy);
-            
-        }
-
-        [Test]
-        public void StandartAsset_OpenMenu_Positive()
+        public void OpenMenu_Positive_MenuOpened()
         {
             var menuButton = new GameObject().FindByUPath("/MainMenuUI(Clone)//OpenMenuButton");
             var menu = new GameObject().FindByUPath("/MainMenuUI(Clone)//MenuParent");
-            //var menuButton = new GameObject().FindByName("MainMenuUI(Clone)", "OpenMenuButton");
-            //var menu = new GameObject().FindByName("MainMenuUI(Clone)", "MenuParent");
 
             menuButton.Should(Be.Present);
             menuButton.Should(Be.ActiveInHierarchy);
@@ -115,6 +29,24 @@ namespace PuppetTesting
 
             menu.Should(Be.Present);
             menu.Should(Be.ActiveInHierarchy);
+        }
+
+        [Test]
+        public void SwitchParticle_Previous_FlareIsActive()
+        {
+            var nextButton = new GameObject().FindByUPath("/UI//Next");
+            var previousButton = new GameObject().FindByUPath("/UI//Previous");
+            var titleLabel = new GameObject().FindByUPath("/UI//TitleText");
+
+            previousButton.Should(Be.Present);
+            previousButton.Should(Be.ActiveInHierarchy);
+            previousButton.Click();
+
+            titleLabel.Should(Be.ActiveInHierarchy);
+            titleLabel.Should(Have.Component("Text"));
+
+            var text = titleLabel.GetComponent("Text");
+            Assert.IsTrue(text.Contains("Flare"));
         }
 
         [TearDown]
---
+++ Puppeteer/Puppeteer/Conditions/Component.cs
@@ -0,0 +1,36 @@
+﻿namespace Puppetry.Puppeteer.Conditions
+{
+    internal class Component : Condition
+    {
+        private string component;
+
+        public Component(string componentName)
+        {
+            component = componentName;
+        }
+
+        public override bool Invoke<T>(T gameObject)
+        {
+            _curentGameObject = gameObject;
+            return gameObject.GetComponent(component) != null;
+        }
+
+        protected override string DescribeExpected()
+        {
+            return $"Has component {component}";
+        }
+
+        protected override string DescribeActual()
+        {
+            return "But does not have it";
+        }
+    }
+
+    public static partial class Have
+    {
+        public static Condition Component(string component)
+        {
+            return new Component(component);
+        }
+    }
+}
